c511699666fd8c1e6929ae5186a6611d255f0737
niaz-morshed-m <niazmorshed14007@gmail.com>
2025-11-12 02:37:50 +0600

    crop adding api created

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,23 +25,21 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = process.env.MONGO_URI;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("krishi_db");
+    const cropsCollection = db.collection("all_crops");
+    
+    app.post("/allCrops", async (req, res) => {
+      const newCrop = req.body;
+      const result = await cropsCollection.insertOne(newCrop);
+      res.send(result);
+    });
+
+    
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +47,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+ 
   }
 }
 run().catch(console.dir);
